e76871438eb8ac72442276c447ea67cc06dc014f
Diva jana <85896704+0g-g0@users.noreply.github.com>
2025-06-30 08:15:33 +0300

    Create int.cpp

---
+++ int.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int age = 21;
+
+	cout << "I'm " << age << "\t"<<"years old";
+}
+
+//In integers we use the command int.
+//When we want to create a space between words we use /t .
