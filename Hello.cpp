a4a831eaf4c8f32938c66e5ae52def176c09669c
Diva jana <85896704+0g-g0@users.noreply.github.com>
2025-06-30 08:36:48 +0300

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -5,5 +5,6 @@ int main()
     cout<< "Hello world\n";
 }
 
-//First programming code "Hallo world"
+// First programming code "Hallo world"
 // \n We use it when we want a new line.
+// We use cout for the information that we want the user to read, meaning it is the output.
