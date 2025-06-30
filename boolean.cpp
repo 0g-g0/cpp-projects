724ac08f47af8786f8558f4336789553e9523407
Diva jana <85896704+0g-g0@users.noreply.github.com>
2025-06-30 08:32:06 +0300

    Create boolean.cpp

---
+++ boolean.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	bool isOpened = true;
+
+	cout << "is opened?" << "\t" << isOpened ;
+}
+
+//Boolean is used in logical things meaning it only contains two answers: true 1 and false 0.
