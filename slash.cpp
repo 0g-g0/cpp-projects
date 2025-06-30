39a70eb8abbeda326b87e1d676f3dbae56f3bdc9
Diva jana <85896704+0g-g0@users.noreply.github.com>
2025-06-30 07:40:51 +0300

    Create slash.cpp

---
+++ slash.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<< "Hello world\n";
+}
+
+//When we use / twice we write notes within one line.
+
+/* 
+"When we use /* at the beginning and the end
+we 
+write notes
+without a specific 
+number of
+lines.
+*/ 
