57a6ce8c286e56f0a72f1850edb3d0cb9b36e2f0
Diva jana <85896704+0g-g0@users.noreply.github.com>
2025-06-30 08:22:55 +0300

    Create float.cpp

---
+++ float.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float price = 56.99;
+
+	cout << "The price of the product is:" << "\t" <<  price ;
+}
+
+//The float command is used when we want a fractional number.
