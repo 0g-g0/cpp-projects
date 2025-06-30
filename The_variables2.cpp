7f574850960e9c862a642b29e3a7450f0a25b779
Diva jana <85896704+0g-g0@users.noreply.github.com>
2025-06-30 07:55:07 +0300

    Create The_variables2.cpp

---
+++ The_variables2.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A = 21;
+	A = 22;
+	cout << "A="<<A;
+}
+
+//When we set another number with the same letter or name as the first variable the program will take the last value.
