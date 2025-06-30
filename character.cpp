811aae58a5397385533b45746422e3b1996fee87
Diva jana <85896704+0g-g0@users.noreply.github.com>
2025-06-30 08:42:06 +0300

    Create character.cpp

---
+++ character.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	char firstletter = 'J';
+    cout << "The first letter of your name is:" << "\n"<< firstletter;
+}
+
+//We use char when we only want a character.
+//Please note that characters must be enclosed in ' '
